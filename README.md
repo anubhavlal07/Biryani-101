eee597d210e04b9ae4b3a89a7615624a0986844e
Anubhav Lal <67056859+anubhavlal07@users.noreply.github.com>
2023-04-06 17:07:46 +0530

    Added the files

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Biryani-101
+
+# Biryani 101
+
+The website features a fun feature that fetches new biryani images every time you click, providing an endless stream of mouth-watering visuals to accompany your biryani exploration. Whether you're a seasoned biryani connoisseur or a curious newbie, this website is your go-to source for all things biryani.
+
+The website that is dedicated to the celebration of biryani - the beloved spiced rice dish from India. The website allows users to explore the world of biryani by providing a visually rich experience.
+
+Built using modern web development technologies such as HTML, CSS, and JavaScript, this website is designed to be fast, responsive, and easy to use. It is fully customizable, with the ability to add new biryani images and tweak the behavior of the image fetching feature.
+
+## Demo
+
+https://anubhavlal07.github.io/Biryani-101/
+
---
+++ index.js
@@ -0,0 +1,51 @@
+let biriyani = document.getElementById("biriyani");
+const btn = document.getElementById("btn");
+
+let getBiriyani = () => {
+  fetch("https://biriyani.anoram.com/get")
+    .then((response) => response.json())
+    .then((data) => {
+      biriyani.innerHTML = `<img src="${data.image}" class="img" alt="...">`;
+    })
+    .catch((error) => console.error(error));
+};
+getBiriyani();
+btn.addEventListener("click", getBiriyani);
+biriyani.addEventListener("click", getBiriyani);
+
+// Dynamic year Footer
+let year = new Date().getFullYear();
+document.getElementById(
+  "footer"
+).innerHTML = `Developed and maintained by <a href="https://github.com/anubhavlal07" target="_blank">Anubhav Lal</a> | &copy; ${year} All Rights Reserved.`;
+
+// Diable input from users
+// (document.onkeydown = function (event) {
+//   if (event.keyCode == 123) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
+//     return false;
+//   } else if (event.ctrlKey && event.keyCode == 85) {
+//     return false;
+//   }
+// }),
+//   false;
+// if (document.addEventListener) {
+//   document.addEventListener(
+//     "contextmenu",
+//     function (e) {
+//       e.preventDefault();
+//     },
+//     false
+//   );
+// } else {
+//   document.attachEvent("oncontextmenu", function () {
+//     window.event.returnValue = false;
+//   });
+// }
